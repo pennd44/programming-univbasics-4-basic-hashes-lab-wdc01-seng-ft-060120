6983ade85dc9d8986f1c0370f848e78ac6860027
Penn M Diba <pdiba@oxbowschool.org>
2020-05-24 19:39:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,31 @@
 def new_hash
   # return an empty hash
+  hash = {}
+  hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  leaves = {
+    spring: "green",
+    summer: "greeeen",
+    fall: "orange",
+    winter: "ded"
+  }
+  leaves
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hasheesh = {
+    :name => 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash = {
+    :id => (number)
+  }
+  hash
 end
